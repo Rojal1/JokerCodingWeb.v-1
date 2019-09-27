e0e059892dbba22f5f1829a7f5e3465a150b615f
Rojal1 <53664740+Rojal1@users.noreply.github.com>
2019-09-27 19:44:36 +0200

    Add files via upload

---
+++ JokerCodingWeb.v-1/app.js
@@ -0,0 +1,27 @@
+const navSlide = () => {
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.nav-links');
+	const navLinks= document.querySelectorAll('nav li');
+	
+	burger.addEventListener('click', () => {
+		nav.classList.toggle('nav-active');
+	
+		burger.classList.toggle('toggle');
+	
+		navLinks.forEach((link, index) => {
+			if (link.style.animation){
+				link.style.animation = '';
+			} else {
+				link.style.animation='navLinkFade 0.5s ease forwards ${index / 7 + 2}s';
+			}
+		});
+	});
+}
+
+setTimeout(function(){
+    document.getElementById('hideIt').className = 'hide';
+}, 3000);
+
+
+
+navSlide();
